cda21179f7e6ff05c4cd9dc191edb58cacbe16c8
Yoan Saint Juste <yoan.saint-juste@epitech.eu>
2022-03-14 03:04:50 +0900

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,156 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
+/**
+ * Base
+ */
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/3.png");
+const matCaps = [];
+matCaps.push(textureLoader.load("/textures/matcaps/7.png"));
+matCaps.push(textureLoader.load("/textures/matcaps/2.png"));
+matCaps.push(textureLoader.load("/textures/matcaps/8.png"));
+matCaps.push(textureLoader.load("/textures/matcaps/5.png"));
+matCaps.push(textureLoader.load("/textures/matcaps/6.png"));
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader();
+
+fontLoader.load("/fonts/Enchanted_Land_Regular.json", (font) => {
+  const textGeometry = new TextGeometry("Yoan", {
+    font,
+    size: 1,
+    height: 0.2,
+    curveSegments: 7,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 6,
+  });
+
+  // Space taken by the geometry
+  // Needed to center
+  textGeometry.computeBoundingBox();
+  textGeometry.center();
+
+  const material = new THREE.MeshMatcapMaterial();
+  material.matcap = matcapTexture;
+  const mesh = new THREE.Mesh(textGeometry, material);
+  scene.add(mesh);
+
+  // Create the geometry only once
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  const donutMaterials = [];
+
+  donutMaterials.push(
+    new THREE.MeshMatcapMaterial(),
+    new THREE.MeshMatcapMaterial(),
+    new THREE.MeshMatcapMaterial(),
+    new THREE.MeshMatcapMaterial(),
+    new THREE.MeshMatcapMaterial()
+  );
+
+  donutMaterials[0].matcap = matCaps[0];
+  donutMaterials[1].matcap = matCaps[1];
+  donutMaterials[2].matcap = matCaps[2];
+  donutMaterials[3].matcap = matCaps[3];
+  donutMaterials[4].matcap = matCaps[4];
+
+  for (let i = 0; i < 500; i++) {
+    const donutMesh = new THREE.Mesh(donutGeometry, donutMaterials[i % 5]);
+    const scale = Math.random() * 2;
+    donutMesh.scale.set(scale, scale, scale);
+    donutMesh.position.x = (Math.random() - 0.5) * 15;
+    donutMesh.position.y = (Math.random() - 0.5) * 15;
+    donutMesh.position.z = (Math.random() - 0.5) * 15;
+    donutMesh.rotation.set(Math.random() * 360, Math.random() * 360, Math.random() * 360);
+    scene.add(donutMesh);
+  }
+});
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("keydown", (ev) => {
+  if (ev.key === "r") {
+    document.location.reload(true);
+  }
+});
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
